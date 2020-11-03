567b4d8f7c04dbda9074e5175eeb43532c03a494
Florian JUDITH <florian.judith.b@gmail.com>
2020-11-03 02:25:59 +0100

    Fixed odbc driver configuration

--- test/import_fakenames.sh
+++ test/import_fakenames.sh
@@ -16,12 +16,15 @@ sudo apt-get install -yq \
 
 sudo pip3 install pyodbc sqlalchemy pymssql pandas
 
-cat << EOF > sudo /etc/odbcinst.ini
+cat << EOF | sudo tee /etc/odbcinst.ini
 [FreeTDS]
 Description=FreeTDS Driver
 Driver=/usr/lib/x86_64-linux-gnu/odbc/libtdsodbc.so
 Setup=/usr/lib/x86_64-linux-gnu/odbc/libtdsS.so
 EOF
+
+sudo cat /etc/odbcinst.ini
+sudo find / -name libtdsodbc.so
 
 pip3 install -r ./test/requirements.txt
 python3 ./test/import_fakenames.py
