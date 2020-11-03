0cc80942e0ae34fbdc386fb0862c2de7669f2a36
Florian JUDITH <florian.judith.b@gmail.com>
2020-11-03 01:29:31 +0100

    Added missing package

--- test/import_fakenames.sh
+++ test/import_fakenames.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+set -e
+
 # Install required pakages
-sudo apt-get install freetds-bin freetds-dev libct4 libsybdb5 tdsodbc unixodbc unixodbc-dev
+sudo apt-get install -yq \
+    python3-setuptools \
+    freetds-bin \
+    freetds-dev \
+    libct4 \
+    libsybdb5 \
+    tdsodbc \
+    unixodbc \
+    unixodbc-dev
+
 sudo pip3 install pyodbc sqlalchemy pymssql pandas
 
 cat << EOF > /etc/odbcinst.ini
