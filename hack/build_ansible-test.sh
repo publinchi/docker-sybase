206ce8ba5c6fab848f231a2443f18b16028dd63f
Florian JUDITH <florian.judith.b@gmail.com>
2020-12-14 01:19:34 +0100

    Fixed push and load may not be set together

--- hack/build_ansible-test.sh
+++ hack/build_ansible-test.sh
@@ -1,2 +1,2 @@
 source ansible-test/.env
-docker image build --progress "plain" -f ansible-test/Dockerfile.ci -t sqlops/sybase-test-container:${SEMVER_TAG} ./
+docker image build --progress "plain" -f ansible-test/Dockerfile.ci -t docker.io/sqlops/sybase-test-container:${SEMVER_TAG} ./
